04bb57bb7a0fd39338bb7ba4a2980a8c778ca8ea
Takuya Kondo <takondo@microsoft.com>
2023-01-23 05:44:59 +0900

    Update PatchWinREScript_2004plus.ps1

--- PatchWinREScript_2004plus.ps1
+++ PatchWinREScript_2004plus.ps1
@@ -1,3 +1,18 @@
+################################################################################################
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+################################################################################################
+
 Param (
 [Parameter(HelpMessage="Work Directory for patch WinRE")][string]$workDir="",
 [Parameter(Mandatory=$true,HelpMessage="Path of target package")][string]$packagePath
